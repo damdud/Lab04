975507a98fe78fa52d71b80e7710e4c33163f7b3
Nikodem Rafalski <nikodem@rafalski.co>
2015-10-27 22:30:51 +0100

    Model danych; narzędzia

---
+++ kod/UamTTA/UamTTA/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace UamTTA
+{
+    public class Account
+    {
+        public Account(string name, decimal balance, string relatedBankAccount = null,
+            decimal? expectedIncomes = null, decimal? targetBalance = null)
+        {
+            Balance = balance;
+            RelatedBankAccount = relatedBankAccount;
+            ExpectedIncomes = expectedIncomes;
+            Name = name;
+            TargetBalance = targetBalance;
+        }
+
+        public string Name { get; set; }
+
+        public string RelatedBankAccount { get; set; }
+
+        public decimal Balance { get; set; }
+
+        public decimal? ExpectedIncomes { get; set; }
+
+        public decimal? TargetBalance { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, RelatedBankAccount: {RelatedBankAccount}, Balance: {Balance}, ExpectedIncomes: {ExpectedIncomes}, TargetBalance: {TargetBalance}";
+        }
+    }
+}
---
+++ kod/UamTTA/UamTTA/Budget.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UamTTA
+{
+    public class Budget
+    {
+        public Budget(
+            DateTime validFrom, DateTime validTo,
+            IEnumerable<Transfer> operations = null, IEnumerable<Account> relatedAccounts = null,
+            Account clearingAccount = null)
+        {
+            ValidFrom = validFrom;
+            ValidTo = validTo;
+            Operations = operations ?? Enumerable.Empty<Transfer>();
+            RelatedAccounts = relatedAccounts ?? Enumerable.Empty<Account>();
+            ClearingAccount = clearingAccount;
+        }
+
+        public DateTime ValidFrom { get; }
+
+        public DateTime ValidTo { get; }
+
+        public IEnumerable<Account> RelatedAccounts { get; }
+
+        public IEnumerable<Transfer> Operations { get; }
+
+        public Account ClearingAccount { get; }
+
+        public override string ToString()
+        {
+            return $"ValidFrom: {ValidFrom}, ValidTo: {ValidTo}, RelatedAccounts: {RelatedAccounts.ToElementsString()}, Operations: {Operations.ToElementsString()}, ClearingAccount: {ClearingAccount}";
+        }
+    }
+}
