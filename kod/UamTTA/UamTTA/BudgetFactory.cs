45db7f694593d0a25254415e03b64001d699043f
Bart Sokol <sokol.bartosz@gmail.com>
2015-10-29 09:38:43 +0100

    Drobny refactoring generatora

--- kod/UamTTA/UamTTA/BudgetFactory.cs
+++ kod/UamTTA/UamTTA/BudgetFactory.cs
@@ -10,15 +10,11 @@ namespace UamTTA
             switch (template.DefaultDuration)
             {
                 case Duration.Weekly:
-                    endDate = startDate.AddDays(6);
+                    endDate = AddWeek(startDate);
                     break;
 
                 case Duration.Monthly:
-                    endDate = startDate.AddMonths(1);
-                    int daysInStartDate = DateTime.DaysInMonth(startDate.Year, startDate.Month);
-                    int daysInNextMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
-                    if (daysInNextMonth >= 30 && (endDate.Day < daysInNextMonth || daysInNextMonth == daysInStartDate))
-                        endDate = endDate.AddDays(-1);
+                    endDate = AddMonth(startDate);
                     break;
 
                 case Duration.Quarterly:
@@ -32,6 +28,21 @@ namespace UamTTA
             }
 
             return new Budget(startDate, endDate);
+        }
+
+        private static DateTime AddWeek(DateTime startDate)
+        {
+            return startDate.AddDays(6);
+        }
+
+        private static DateTime AddMonth(DateTime startDate)
+        {
+            DateTime endDate = startDate.AddMonths(1);
+            int daysInStartDate = DateTime.DaysInMonth(startDate.Year, startDate.Month);
+            int daysInNextMonth = DateTime.DaysInMonth(endDate.Year, endDate.Month);
+            if (daysInNextMonth >= 30 && (endDate.Day < daysInNextMonth || daysInNextMonth == daysInStartDate))
+                endDate = endDate.AddDays(-1);
+            return endDate;
         }
     }
 }
